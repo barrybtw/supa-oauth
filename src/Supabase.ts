9239d23fb8185a924142f4be6e98e2f51aef5db6
Nicolai Christensen <nicolai.bernhard03@gmail.com>
2022-09-15 16:06:39 +0200

    Update Supabase.ts

--- src/Supabase.ts
+++ src/Supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://gwwdzohqhvbkxdnhmrub.supabase.co";
+const supabaseUrl = "https://esvelcruuzbwiupiepml.supabase.co";
 const supabaseKey = import.meta.env.VITE_APP_SUPABASEKEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
